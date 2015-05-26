e86af8a0d1dca509b64984aed779aef626bc0236
peterSW <peter.stroia-willia1@192.168.0.6>
2015-05-26 01:51:47 +0100

    Add helper function to ProjectAction to get its projects parameter names.

--- src/main/java/org/jenkinsci/plugins/buildfinder/ProjectAction.java
+++ src/main/java/org/jenkinsci/plugins/buildfinder/ProjectAction.java
@@ -2,6 +2,9 @@ package org.jenkinsci.plugins.buildfinder;
 
 import hudson.model.AbstractProject;
 import hudson.model.Action;
+import hudson.model.ParametersDefinitionProperty;
+import java.util.ArrayList;
+import java.util.List;
 import org.kohsuke.stapler.export.ExportedBean;
 
 @ExportedBean(defaultVisibility = -1)
@@ -29,6 +32,17 @@ class ProjectAction implements Action {
     
     public AbstractProject getProject() {
         return parentProject;
+    }
+    
+    public List<String> getParameterNames()
+    {
+        ParametersDefinitionProperty property = (ParametersDefinitionProperty)parentProject.getProperty(ParametersDefinitionProperty.class);
+        if(property == null) {
+            return new ArrayList<String>();
+        }
+        else {
+            return property.getParameterDefinitionNames();
+        }
     }
 
 }
--- src/test/java/org/jenkinsci/plugins/buildfinder/ProjectActionTest.java
+++ src/test/java/org/jenkinsci/plugins/buildfinder/ProjectActionTest.java
@@ -1,13 +1,22 @@
 package org.jenkinsci.plugins.buildfinder;
 
 import hudson.model.AbstractProject;
+import hudson.model.ParameterDefinition;
+import hudson.model.ParametersDefinitionProperty;
+import hudson.model.StringParameterDefinition;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.empty;
 import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.jvnet.hudson.test.JenkinsRule;
 import static org.mockito.Mockito.mock;
 
 public class ProjectActionTest {
-    
+    @Rule public JenkinsRule j = new JenkinsRule();
     AbstractProject project;
     ProjectAction action;
     
@@ -34,6 +43,25 @@ public class ProjectActionTest {
     @Test
     public void testGetProjectGivesActionsProject() {
         assertThat(action.getProject(), is(project));
+    }
+    
+    @Test
+    public void testGetParameterNamesGivesEmptyWhenThereAreNoParameters() {
+        assertThat(action.getParameterNames(), is(empty()));
+    }
+
+    @Test
+    public void testGetParameterNamesGivesListOfParameterNames() throws IOException {
+        project = j.createFreeStyleProject();
+        action = new ProjectAction(project);
+        addAParametersTo(project);
+        assertThat(action.getParameterNames().get(0), equalTo("parameter"));
+    }
+
+    private void addAParametersTo(AbstractProject project) throws IOException {
+        List<ParameterDefinition> paramsToAdd = new ArrayList<ParameterDefinition>();
+        paramsToAdd.add(new StringParameterDefinition("parameter", "what ever"));
+        project.addProperty(new ParametersDefinitionProperty(paramsToAdd));
     }
     
 }
